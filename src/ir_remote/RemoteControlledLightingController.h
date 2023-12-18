b0d13d6130b73c95b76deb890c22eaa18f9cef56
Tristan Pearce <tristanpearce24@gmail.com>
2023-12-18 16:45:45 +0000

    add IR header

---
+++ src/ir_remote/RemoteControlledLightingController.h
@@ -0,0 +1,11 @@
+#include "../lighting/LightingController.h"
+
+class RemoteControlledLightingController : public LightingController {
+
+public:
+
+
+private:
+
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -23,53 +23,7 @@
 
 #include "implementation/WS2812/WS2812LedCollection.h"
 
-
-void angleToColor(float angle, uint* out_r, uint* out_g, uint* out_b,float brightness = 0.2f) {
-    // Normalize angle to be within [0, 360)
-    angle = fmod(angle, 360.0);
-    if (angle < 0) {
-        angle += 360.0;
-    }
-
-    // Convert HSL to RGB
-    float c = 1.0;
-    float x = c * (1.0 - std::abs(fmod(angle / 60.0, 2.0) - 1.0));
-    float m = 0.0;
-    
-    float r, g, b;
-    if (angle < 60.0) {
-        r = c;
-        g = x;
-        b = 0.0;
-    } else if (angle < 120.0) {
-        r = x;
-        g = c;
-        b = 0.0;
-    } else if (angle < 180.0) {
-        r = 0.0;
-        g = c;
-        b = x;
-    } else if (angle < 240.0) {
-        r = 0.0;
-        g = x;
-        b = c;
-    } else if (angle < 300.0) {
-        r = x;
-        g = 0.0;
-        b = c;
-    } else {
-        r = c;
-        g = 0.0;
-        b = x;
-    }
-
-    // Convert floats to RGB range (0-255)
-    *out_r = (uint)((r * brightness) * 255.0f);
-    *out_g = (uint)((g * brightness) * 255.0f);
-    *out_b = (uint)((b * brightness) * 255.0f);
-}
-
-
+#include "ir_remote/IR.h"
 
 const int NUMBER_OF_LEDS = 300;
 
@@ -89,8 +43,10 @@ int main() {
     LightingController* controller_ptr;
     controller_ptr = new SingleProgramLightingController(program_ptr, leds_ptr, false);
 
-    controller_ptr->Enable();
+    //controller_ptr->Enable();
     
+
+
     delete controller_ptr;
     delete program_ptr;
     delete leds_ptr;
