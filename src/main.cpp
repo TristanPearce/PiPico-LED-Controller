d59a3d7e28b66497aac137db88c80e16e07f0303
Tristan Pearce <tristanpearce24@gmail.com>
2023-12-19 12:37:23 +0000

    add programs

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,13 @@
 
 #include "lighting/LightingController.h"
 #include "lighting/controllers/SingleProgramLightingController.h"
+#include "lighting/controllers/ButtonDrivenLightingController.h"
 
 #include "lighting/LightingProgram.h"
 #include "lighting/programs/ColourWheelLightingProgram.h"
 #include "lighting/programs/ChristmasLightingProgram.h"
+
+#include "programs/xmas/XmasRunnerLightingProgram.h"
 
 #include "lighting/Colour.h"
 
@@ -83,16 +86,31 @@ int main() {
     LedCollection* leds_ptr;
     leds_ptr = new WS2812LedCollection(NUMBER_OF_LEDS, WS2812_PIN);
 
-    LightingProgram* program_ptr;
-    program_ptr = new ChristmasLightingProgram();
+    LightingProgram* christmasLightingProgram;
+    christmasLightingProgram = new ChristmasLightingProgram();
+
+    LightingProgram* xmasRunnerProgram;
+    xmasRunnerProgram = new XmasRunnerLightingProgram();
+
+    LightingProgram* colourWheelLightingProgram;
+    colourWheelLightingProgram = new ColourWheelLightingProgram();
+
+    std::vector<LightingProgram*> programs = std::vector<LightingProgram*>();
+    programs.push_back(christmasLightingProgram);
+    programs.push_back(xmasRunnerProgram);
+    programs.push_back(colourWheelLightingProgram);
 
     LightingController* controller_ptr;
-    controller_ptr = new SingleProgramLightingController(program_ptr, leds_ptr, false);
+    controller_ptr = new ButtonDrivenLightingController(programs, leds_ptr);
 
     controller_ptr->Enable();
     
     delete controller_ptr;
-    delete program_ptr;
+
+    delete christmasLightingProgram;
+    delete xmasRunnerProgram;
+    delete colourWheelLightingProgram;
+
     delete leds_ptr;
 
     return 0;
